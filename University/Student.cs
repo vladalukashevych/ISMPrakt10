417f569624e848887fde60f238befc6af51a5dd1
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-01-26 13:26:07 +0200

    Implemented class Student

--- University/Student.cs
+++ University/Student.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace University
 {
-    class Student
+    class Student : Person
     {
+        protected int Grade { set; get; }
+        protected string Group { set; get; }
+        protected string Faculty { set; get; }
+        protected string EducationalInstitution { set; get; }
+
+        public Student(string firstName, string lastName, DateTime dateOfBirth, int grade, string group,
+            string faculty, string educationalInstitution) : base(firstName, lastName, dateOfBirth)
+        {
+            Grade = grade;
+            Group = group;
+            Faculty = faculty;
+            EducationalInstitution = educationalInstitution;
+        }
+
+        public string GetStudentMainInfo()
+        {
+            return LastName + " " + FirstName + " - " + Group;
+        }
     }
 }
