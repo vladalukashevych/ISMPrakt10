22b70181e54fb8cdf9166782d2078025a29ba80d
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-01-26 13:13:30 +0200

    Implemented class Professor

--- University/Professor.cs
+++ University/Professor.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace University
 {
-    class Professor
+    class Professor : Person
     {
+        protected string Position { set; get; }
+        protected string Department { set; get; }
+        protected string EducationalInstitution { set; get; }
+
+        public Professor(string firstName, string lastName, DateTime dateOfBirth, string position, 
+            string department, string educationalInstitution): base(firstName, lastName, dateOfBirth)
+        {
+            Position = position;
+            Department = department;
+            EducationalInstitution = educationalInstitution;
+        }
+
+        public string GetDepartment()
+        {
+            return Department;
+        }
     }
 }
