e5bc38fef1a51eaae8bc7ffb5ef1155d5e750081
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-01-26 11:25:21 +0200

    Implemented class Person

--- University/Person.cs
+++ University/Person.cs
@@ -8,5 +8,21 @@ namespace University
 {
     public class Person
     {
+        protected string FirstName { set; get; }
+        protected string LastName { set; get; }
+        protected DateTime DateOfBirth { set; get; }
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public int GetAge()
+        {
+            TimeSpan age = DateTime.Now - DateOfBirth;
+            return (int)(age.TotalDays/365);
+        }
     }
 }
