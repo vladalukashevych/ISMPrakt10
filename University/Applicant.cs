2056208f64ab5f5045b92de1c5d4c82b2a37a8cc
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-01-26 12:43:24 +0200

    Implemented class Applicant

--- University/Applicant.cs
+++ University/Applicant.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace University
 {
-    class Applicant
+    public class Applicant : Person
     {
+        protected int ZNOMathScore { set; get; }
+        protected int ZNOUkrainianScore { set; get; }
+        protected int ZNOOptionalScore { set; get; }
+        protected float SchoolCertificateGrade { set; get; }
+        protected string SchoolName;
+
+        public Applicant() : base()
+        {
+            ZNOMathScore = 0;
+            ZNOUkrainianScore = 0;
+            ZNOOptionalScore = 0;
+            SchoolCertificateGrade = 0;
+            SchoolName = "none";
+        }
+
+        public Applicant(string firstName, string lastName, DateTime dateOfBirth, int znoMathScore, 
+            int znoUkrainianScore, int znoOptionalScore, float schoolCertificateGrade, string schoolName) 
+            : base(firstName, lastName, dateOfBirth)
+        {
+            ZNOMathScore = znoMathScore;
+            ZNOUkrainianScore = znoUkrainianScore;
+            ZNOOptionalScore = znoOptionalScore;
+            SchoolCertificateGrade = schoolCertificateGrade;
+            SchoolName = schoolName;
+        }
+
+        public double GetCompetetiveScore(float mathCoefficient, float ukraineanCoefficient, float optionalCoefficient)
+        {
+            return ZNOMathScore * mathCoefficient + ZNOUkrainianScore * ukraineanCoefficient + ZNOOptionalScore * optionalCoefficient;
+        }
     }
 }
